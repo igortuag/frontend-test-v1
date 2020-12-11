61b21eb696f2c49d4c650fbf7a66b17bd6638afe
Igor Tuag <igortuag@gmail.com>
2020-12-11 15:08:42 -0300

    header responsive

--- src/components/HeaderLayout/index.jsx
+++ src/components/HeaderLayout/index.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/img/logo.svg';
 import { ReactComponent as User } from '../../assets/img/ic-user.svg';
 import { ReactComponent as Cart } from '../../assets/img/ic-cart.svg';
+import { ReactComponent as Menu } from '../../assets/img/ic-menu.svg';
 import SearchInput from '../SearchInput';
 import NavMenu from '../NavMenu';
 
@@ -12,33 +13,53 @@ import './styles.scss';
 function HeaderLayout() {
   const { products } = useSelector(state => state.cart);
 
+  const handleActiveMenu = () => {
+    document.querySelector('#navMenu').classList.toggle('header__nav--active');
+  };
+
   return (
     <header>
       <div className="header container">
-        <a href="#">
+        <button
+          type="button"
+          onClick={() => handleActiveMenu()}
+          className="menu-mobile"
+        >
+          <Menu />
+        </button>
+        <a href="#" className="header__logo">
           <Logo />
         </a>
-        <SearchInput />
+        <div className="header__search">
+          <SearchInput />
+        </div>
         <a href="#" className="header__link header__link--disable">
-          LOCATION
+          <p className="header__link__description">LOCATION</p>
         </a>
         <a href="#" className="header__link header__link--disable">
-          Help & Contact
+          <p className="header__link__description">Help & Contact</p>
         </a>
         <a href="#" className="header__link">
           <div className="header__icon">
             <User />
           </div>
-          My Account
+          <p className="header__link__description">My Account</p>
         </a>
         <a href="#" className="header__link">
           <div className="header__icon">
             <Cart />
           </div>
-          {`(${products})`}
+          <p className="header__link__description">{`(${products})`}</p>
         </a>
       </div>
-      <NavMenu />
+      <div className="header__nav">
+        <NavMenu />
+      </div>
+      <div className="header__nav header__nav--mobile" id="navMenu">
+        <button type="button" onClick={() => handleActiveMenu()}>
+          <NavMenu />
+        </button>
+      </div>
     </header>
   );
 }
